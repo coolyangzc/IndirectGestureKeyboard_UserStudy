e14fdc203e0b45fd34741f3203e392d14b3527a0
coolyangzc <yang-zc13@mails.tsinghua.edu.cn>
2017-05-12 15:43:45 +0800

    Add corpus-noduplicate

--- Analyse/corpus/clean.py
+++ Analyse/corpus/clean.py
@@ -1,8 +1,16 @@
 fd = file('ANC-written-count.txt')
-outfd = open('corpus-written.txt', 'w')
+outfd = open('corpus-written-noduplicate.txt', 'w')
+dict = {}
 lines = fd.readlines()
 for line in lines:
     line_data = line.split('\t');
     word = line_data[0]
-    if word.isalpha():
-        outfd.write(word + '    ' + line_data[-1]);
+    if not word.isalpha():
+        continue
+    if dict.has_key(word):
+        dict[word] += int(line_data[-1])
+    else:
+        dict[word] = int(line_data[-1])
+sorted_dict = sorted(dict.items(), key=lambda item:item[1], reverse=True)
+for word in sorted_dict:
+    outfd.write(word[0] + ' ' + str(word[1]) + '\n')
