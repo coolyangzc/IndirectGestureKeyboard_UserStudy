5208df619f4220114620b1c92fa95200c64a40be
coolyangzc <yang-zc17@mails.tsinghua.edu.cn>
2018-09-02 10:46:46 +0800

    Study 4: Heat Clean

--- Study4/Heat-Analyzer.cpp
+++ Study4/Heat-Analyzer.cpp
@@ -16,8 +16,11 @@ const int USER_L = 1;
 double dtw[MAXSAMPLE][MAXSAMPLE];
 int s[26], t[26], tot;
 double sx[2][26], sy[2][26], tx[2][26], ty[2][26], scnt[2][26], tcnt[2][26];
+vector<Vector2> cor[2][26][2];
+
+
 string userID;
-fstream heatFout, centerFout;
+fstream heatFout, centerFout, cleanFout;
 
 void init()
 {
@@ -28,6 +31,9 @@ void init()
     string centerFileName = "res/Heat_Center_Study2.csv";
     centerFout.open(centerFileName.c_str(), fstream::out);
     centerFout << "id,usage,type,key,x,y" << endl;
+
+    cleanFout.open("res/Heat_Clean_Study2.csv", fstream::out);
+    cleanFout << "usage,type,key,x,y" << endl;
 
     initKeyboard(dtw);
 }
@@ -59,6 +65,9 @@ void calcHeat(int id)
         s[sc]++; t[tc]++;
         scnt[p][sc] ++; tcnt[p][tc]++;
 
+        cor[p][sc][0].push_back(Vector2(rawstroke.front().x, rawstroke.front().y));
+        cor[p][tc][1].push_back(Vector2(rawstroke.back().x, rawstroke.back().y));
+
         sx[p][sc] += rawstroke.front().x;
         sy[p][sc] += rawstroke.front().y;
 
@@ -83,6 +92,83 @@ void calcHeat(int id)
             << rawstroke.back().y << ","
             << endl;
     }
+}
+
+void calcUserHeat()
+{
+    rep(p, 2)
+        rep(i, 26)
+        {
+            sx[p][i] /= scnt[p][i];
+            sy[p][i] /= scnt[p][i];
+            tx[p][i] /= tcnt[p][i];
+            ty[p][i] /= tcnt[p][i];
+            string usage = (p==0)?"Indirect":"Direct";
+            if (scnt[p][i] > 0)
+                centerFout  << userID << ","
+                            << usage << ","
+                            << "start,"
+                            << (char)(i + 'a') << ","
+                            << sx[p][i] << ","
+                            << sy[p][i]
+                            << endl;
+
+            if (tcnt[p][i] > 0)
+                centerFout  << userID << ","
+                            << usage << ","
+                            << "end,"
+                            << (char)(i + 'a') << ","
+                            << tx[p][i] << ","
+                            << ty[p][i]
+                            << endl;
+        }
+    memset(sx, 0, sizeof(sx));
+    memset(tx, 0, sizeof(tx));
+    memset(sy, 0, sizeof(sy));
+    memset(ty, 0, sizeof(ty));
+    memset(scnt, 0, sizeof(scnt));
+    memset(tcnt, 0, sizeof(tcnt));
+}
+
+void calcCleanHeat()
+{
+    int tot = 0, used = 0;
+    rep(p, 2)
+        rep(c, 26)
+            rep(t, 2)
+            {
+                vector<Vector2>& v = cor[p][c][t];
+                int n = v.size();
+                if (n <= 15)
+                    continue;
+                double x = 0, y = 0, sx = 0, sy = 0;
+                rep(i, n)
+                {
+                    x += v[i].x; y += v[i].y;
+                }
+                x /= n; y /= n;
+
+                rep(i, n)
+                {
+                    sx += sqr(v[i].x - x);
+                    sy += sqr(v[i].y - y);
+                }
+                sx = sqrt(sx / (n - 1));
+                sy = sqrt(sy / (n - 1));
+                string usage = (p==0)?"Indirect":"Direct";
+                string type = (t==0)?"start":"end";
+                cout << usage << "," << type << "," << (char)(c + 'a') << "," << n
+                                   << "," << x << "," << y << "," << sx << "," << sy << endl;
+                rep(i, n)
+                    if (abs(v[i].x - x) < 3 * sx && abs(v[i].y - y) < 3 * sy)
+                    {
+                        used++;
+                        cleanFout << usage << "," << type << "," << (char)(c + 'a')
+                                  << "," << v[i].x << "," << v[i].y << endl;
+                    }
+                tot += n;
+            }
+    cout << used << "/" << tot << endl;
 }
 
 stringstream ss;
@@ -112,43 +198,14 @@ int main()
             readData(fileName, i);
             calcHeat(i);
         }
-        rep(p, 2)
-            rep(i, 26)
-            {
-                sx[p][i] /= scnt[p][i];
-                sy[p][i] /= scnt[p][i];
-                tx[p][i] /= tcnt[p][i];
-                ty[p][i] /= tcnt[p][i];
-                string usage = (p==0)?"Indirect":"Direct";
-                if (scnt[p][i] > 0)
-                    centerFout  << userID << ","
-                                << usage << ","
-                                << "start,"
-                                << (char)(i + 'a') << ","
-                                << sx[p][i] << ","
-                                << sy[p][i]
-                                << endl;
-
-                if (tcnt[p][i] > 0)
-                    centerFout  << userID << ","
-                                << usage << ","
-                                << "end,"
-                                << (char)(i + 'a') << ","
-                                << tx[p][i] << ","
-                                << ty[p][i]
-                                << endl;
-            }
-        memset(sx, 0, sizeof(sx));
-        memset(tx, 0, sizeof(tx));
-        memset(sy, 0, sizeof(sy));
-        memset(ty, 0, sizeof(ty));
-        memset(scnt, 0, sizeof(scnt));
-        memset(tcnt, 0, sizeof(tcnt));
+        calcUserHeat();
     }
+    calcCleanHeat();
     rep(i, 26)
         printf("%c: %d, %.4lf%%; %d, %.4lf%%\n", i + 'a',
                s[i], (double)s[i] / tot * 100, t[i], (double)t[i] / tot * 100);
     heatFout.close();
+    cleanFout.close();
     centerFout.close();
     return 0;
 }
