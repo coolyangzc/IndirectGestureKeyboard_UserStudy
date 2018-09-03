3955d84f1dd8a04cf3e4180a906d6514eaa0c00c
coolyangzc <yang-zc17@mails.tsinghua.edu.cn>
2018-09-03 15:43:58 +0800

    Study 5: All user data

--- Study5/Common.h
+++ Study5/Common.h
@@ -19,12 +19,12 @@ const double eps = 1e-6;
 const double INF = 1e10;
 const int LEXICON_SIZE = 10000;
 
-const int USER_NUM = 23;
+const int USER_NUM = 24;
 const std::string user[USER_NUM + 1] =
-{"Null", "yzc", "zc", "gyz", "yyk", "yezp", "lxh", "wxy", "guyz", "plh", "maye", "cxs", "lzn",
+{"Null", "yzc", "zc", "gyz", "yyk", "yezp", "lxh", "wxy", "guyz", "lzx", "maye", "cxs", "lzn",
 //   0      1     2      3      4       5      6      7       8      9      10     11     12
- "lyq", "hhs", "xxh", "swn", "wrl", "fsy", "rj", "ljh", "yk", "sk", "lgh"};
-// 13     14     15     16     17     18    19     20    21    22     23
+ "lyq", "hhs", "xxh", "swn", "wrl", "fsy", "rj", "ljh", "yk", "sk", "lgh", "shuyi"};
+// 13     14     15     16     17     18    19     20    21    22     23       24
 
 int Random(int mo)
 {
